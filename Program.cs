d3b83d698acd2176ac7365c2c2840fd69b489b80
David Nokto <david.nokto@gmail.com>
2022-11-08 15:46:47 +0100

    Davids förändringar

--- Program.cs
+++ Program.cs
@@ -1,44 +1,53 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
-using System.Diagnostics.SymbolStore;
-
-Console.WriteLine("Huvudmeny!Ni trycker Program1 Program2 Program3 for olika funktioner ");
-
-namespace exercise2
+﻿namespace exercise2
 {
-    class switchchoice
+    class Program
     {
         public static void Main()
         {
-            Console.WriteLine("four choice 0 1 2 3 4");
-            Consile.Write("your choice");
-            string s = Console.Readline();
-            int n = int.Parse(s);
-            switch(n)
+            do
             {
-                case 0:
-                    Console.WriteLine("det är felaktig input");
-                    break;
-                case 1:
-                    Program1;
-                case 2:
-                    Program2;
-                case 3;
-                    Program3;
-                case 4:
-                    Program4;
-            }
+                Console.Clear();
+                Console.WriteLine("Huvudmeny!Ni trycker Program1 Program2 Program3 for olika funktioner ");
+                Console.WriteLine("four choice 0 1 2 3 4");
+                Console.Write("your choice");
+                string s = Console.ReadLine();
+                int n = int.Parse(s);
+                switch (n)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        TicketPrice();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        GroupTicketPrice();
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        RepeatTen();
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        FindThirdWord();
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Felaktig input");
+                        break;
+                }
+
+            } while (true);
+
         }
-    }
-    class Program1
-    {
-        static void Main(string[] args)
+
+
+
+        static void TicketPrice()
         {
+
             Console.WriteLine("din ålder i siffror");
             int num = Convert.ToInt32(Console.ReadLine());
 
@@ -54,46 +63,43 @@ namespace exercise2
             {
                 Console.WriteLine("Standardpris: 120kr");
             }
-        }
-    }
 
-    class Program2
-    {
-        static void Main(string[] args)
+        }
+        static void GroupTicketPrice()
         {
-            Console.WriteLine("hur många vi är som ska gå på bio");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int i = 1;
+
+            Console.WriteLine("Hur många vi är som ska gå på bio");
+            int numberOfTickets = Convert.ToInt32(Console.ReadLine());
+
             int t = 0;
             int sum = 0;
-            for (; i<= num;)
+            for (var i = 0; i < numberOfTickets; i++)
             {
-                i++;
-                if (num < 20)
+                Console.WriteLine($"Ålder på besökare nr. {i + 1}");
+                var age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 20)
                 {
-                    t=80;
+                    t = 80;
                 }
-                else if (num > 64)
+                else if (age > 64)
                 {
-                    t=90;
+                    t = 90;
                 }
                 else
                 {
-                    t=120;
+                    t = 120;
                 }
 
                 sum = sum + t;
-                 }
-                
-             Console.WriteLine("total kostnad="+sum);
-            
             }
+
+            Console.WriteLine("total kostnad=" + sum);
+
         }
-    }
-    class Program3
-    {
-        static void Main(string[] args)
+        static void RepeatTen()
         {
+
             string meningen = Console.ReadLine();
             int num = 1;
             for (; num <= 10;)
@@ -101,19 +107,24 @@ namespace exercise2
                 Console.WriteLine(meningen);
                 num++;
             }
+
         }
-    }
-    class Program4
-    {
-        static void Main(string[] args)
+        static void FindThirdWord()
         {
+
             Console.WriteLine("anger en mening med minst 3 ord");
             string words = Console.ReadLine();
-            string[] split = words.Split(new char[] {' '});
+            string[] split = words.Split(new char[] { ' ' });
             foreach (string s in split)
             {
                 Console.WriteLine(s + "\n");
             }
+
         }
+
+
     }
+
+
 }
+
